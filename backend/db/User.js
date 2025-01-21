3a8b608fe46537ed16e467039b3219018a12feef
Rahul <rahulrawat4060@gmail.com>
2025-01-21 20:40:05 +0530

    Fix mongoose connection and clean up signup route code

--- backend/db/User.js
+++ backend/db/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL);
 
 const userSchema = new mongoose.Schema({
     username: {
--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -1,9 +1,8 @@
-const { User } = require("../db/User");
+const User = require("../db/User");
 const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res) => {
   const { username, email, password } = req.body;
-
   try {
     const isExist = await User.find({ username: username });
     if (isExist.length > 0) {
@@ -22,7 +21,6 @@ exports.signup = async (req, res) => {
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (e) {
- 
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
