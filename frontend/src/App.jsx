f261357cdb78ef214ce10a02cc320e8102e38e70
Rahul <rahulrawat4060@gmail.com>
2025-01-22 23:41:45 +0530

    navigation structure on components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,22 @@
-import { useState } from 'react'
-import Signup from './components/Signup'
- import Login from './components/Login'
- import UploadImage from './components/Uploadimage'
- import ImageDecryption from './components/ImageDecryption'
+import { useState } from "react";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import UploadImage from "./components/Uploadimage";
+import ImageDecryption from "./components/ImageDecryption";
+import { NavLink } from "react-router";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Welcome to Pixel Storage</h1>
-
-      <Signup/>
-      <br/>
-      <br/>
-      <br/>
-      <Login/>
-      <br/>
-      <br/>
-      <br/>
-      <UploadImage/>
-      <br/>
-      <br/>
-      <br/>
-      <ImageDecryption/>
+      <nav>
+        <Signup />
+        <Login />
+        <UploadImage />
+        <ImageDecryption />
+      </nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
