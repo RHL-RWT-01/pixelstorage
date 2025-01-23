78f5bb71e1eb205e03e9207e27b159555dc4d0a7
Rahul <rahulrawat4060@gmail.com>
2025-01-23 21:47:28 +0530

    Refactor Home component styles and integrate Tailwind CSS

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,17 +3,13 @@ import "tailwindcss";
 function Home() {
   return (
     <>
-    <div style={{ 
-        height: '1000px',
-        width: '100%',
-        backgroundColor: "rgba(48, 35, 219, 0.8)",
-    }} >
+    <div className='size-100 bg-pink-700 text-white'>
       <h1 >Welcome to Pixel Storage</h1>
       <p >
         Pixel Storage is a secure and efficient image storage solution that allows you to store,
         share, and retrieve images with high security and privacy.
       </p>
-      <button>Get Started</button>
+      <button  >Get Started</button>
       </div>
     </>
   )
